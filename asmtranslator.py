d3902ab5d86359af4a6395e480dde3650801a401
Alexander Zuev <zuev08@gmail.com>
2017-12-16 21:00:10 +0300

    Switch

--- asmtranslator.py
+++ asmtranslator.py
@@ -79,6 +79,80 @@ class AsmSyntax:
         return "JNZ " + label
 
 
+class AsmCommand:
+
+    def __init__(self, cmd):
+        self.cmd = cmd
+
+    def __str__(self):
+        return self.cmd
+
+
+class AsmLabel:
+
+    def __init__(self, label):
+        self.label = label
+
+    def __repr__(self):
+        return self.label
+
+
+class FutureLabel:
+
+    def __init__(self):
+        self.label = None
+
+    def set_label(self, label):
+        self.label = label
+
+    def get_label(self):
+        return self.label
+
+    def __repr__(self):
+        if self.label is None:
+            return "undefined"
+        else:
+            return str(self.label)
+
+    def __str__(self):
+        return repr(self)
+
+
+class SwitchToken:
+
+    def __init__(self, insert_pos):
+        self.insert_pos = insert_pos
+        self.end_label = FutureLabel()
+
+    def __repr__(self):
+        return "<switch insert_pos={} end={}>".format(self.insert_pos, self.end_label)
+
+    def get_insert_pos(self):
+        return self.insert_pos
+
+    def get_end_label(self):
+        return self.end_label
+
+
+class JmpToken:
+
+    def __init__(self, label):
+        self.label = label
+
+    def __repr__(self):
+        return "JMP {}".format(self.label)
+
+
+class CmpToken:
+
+    def __init__(self, constant, je_label):
+        self.constant = constant
+        self.je_label = je_label
+
+    def __repr__(self):
+        return "<CMP {}, JE {}>".format(self.constant, self.je_label)
+
+
 class AsmTranslator:
 
     def __init__(self, non_operators):
@@ -132,6 +206,7 @@ class AsmTranslator:
             "*" : 12,
             "/" : 12,
         }
+        self.asm_syntax = AsmSyntax()
 
     def to_asm(self, token_list):
         result_list = []
@@ -169,15 +244,45 @@ class AsmTranslator:
                     token_list.pop(0)
                 elif current_value == ";" or current_value == "end":
                     pop_until_any_of("for", "while", "switch", "case", "=", extra_pop=False)
-                    result_list.append(op_stack.pop())
+                    if len(op_stack) > 0:
+                        result_list.append(op_stack.pop())
                     token_list.pop(0)
                 elif current_value == "case":
-                    pop_until("{", extra_pop=False)
-                    # do not pop '{'
-                    # result_list.append(op_stack.pop())
-                    token_list.pop(0)
+                    token_list.pop(0) # case
+                    # find nearest "<switch>"
+                    s = None
+                    for t in reversed(op_stack):
+                        if type(t) is SwitchToken:
+                            s = t
+                            break
+                    pop_until(s, extra_pop=False)
+                    result_list.append(JmpToken(s.get_end_label()))
+                    case_label = self.asm_syntax.get_label_for("case")
+                    constant = token_list.pop(0).value
+                    result_list.append(AsmLabel(case_label))
+                    result_list.insert(s.get_insert_pos(), CmpToken(constant, case_label))
+
                 elif current_value == "}":
-                    pop_until("{")
+                    # find nearest "<switch>"
+                    s = None
+                    for t in reversed(op_stack):
+                        if type(t) is SwitchToken:
+                            s = t
+                            break
+                    if s is not None:
+                        pop_until_any_of(s, "{", extra_pop=False)
+
+                        if len(op_stack) > 0 and op_stack[-1] == "{":
+                            op_stack.pop()
+                        else:
+                            # finish "<switch>"
+                            end_label = AsmLabel(self.asm_syntax.get_label_for("switch_end"))
+                            result_list.append(end_label)
+                            s.get_end_label().set_label(end_label)
+
+                    else:
+                        # no "<switch>"
+                        pop_until("{")
                     token_list.pop(0)
                 elif current_value == ":":
                     token_list.pop(0)
@@ -186,7 +291,10 @@ class AsmTranslator:
                 current_priority = self.input_priority[current_value]
 
                 if len(op_stack) > 0:
-                    stack_priority = self.op_stack_priority[op_stack[-1]]
+                    last_op = op_stack[-1]
+                    stack_priority = -1
+                    if type(last_op) is str:
+                        stack_priority = self.op_stack_priority[last_op]
 
                     if current_priority > stack_priority:
                         token_list.pop(0)
@@ -198,6 +306,11 @@ class AsmTranslator:
                 else:
                     token_list.pop(0)
                     op_stack.append(current_value)
+
+            if op_stack[-2:] == ["switch", "{"]:
+                op_stack.pop() # {
+                op_stack.pop() # switch
+                op_stack.append(SwitchToken(len(result_list)))
 
         print(result_list, op_stack, list(map(lambda x: x.value, token_list)), sep='\n')
         return result_list
